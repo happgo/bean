3dd563c08de373bb58a0424313e91f223041183e
zhangbofa <626223818@qq.com>
2019-09-18 17:33:55 +0800

    init

---
+++ src/Annotation/Bean.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA
+ * Author: 张伯发
+ * Date  : 2019/9/10
+ * Time  : 17:27
+ */
+
+namespace Happgo\Bean\Annotation;
+
+
+final class Bean
+{
+    public function say()
+    {
+        var_dump('hello');
+    }
+
+
+
+}
